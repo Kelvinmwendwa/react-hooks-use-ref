bb63db1c513ebb0cefe7f8f2c478827d3aeb1006
Kelvin <kmwendwa476@gmail.com>
2022-07-27 00:12:52 +0300

    Completed code along

--- src/components/CounterRef.js
+++ src/components/CounterRef.js
@@ -4,8 +4,8 @@ function CounterRef() {
   const count = useRef(0);
 
   function handleClick() {
-    count.current = count.current + 1;
     console.log(count.current);
+    return count.current = count.current + 1;
   }
 
   return (
